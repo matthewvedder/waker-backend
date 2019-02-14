4bfa405efbf97d780c9afd9323c3a804423fae29
Matthew Vedder <matthewvedder@gmail.com>
2019-02-14 18:21:13 -0500

    add aws credentials

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
 
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
-  config.active_storage.service = :local
+  config.active_storage.service = :amazon
 
 
   # Raises error for missing translations
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,7 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.active_storage.service = :amazon
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
