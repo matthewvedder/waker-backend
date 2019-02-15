ffcafa70ede5bda9a7ff00abbcd9ec8d1a7a8a2b
Matthew Vedder <matthewvedder@gmail.com>
2019-02-14 19:44:12 -0500

    fix storage yml file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,6 +4,7 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
+
   config.cache_classes = false
 
   # Do not eager load code on boot.
@@ -36,8 +37,7 @@ Rails.application.configure do
 
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
-  config.active_storage.service = :amazon
-
+  config.active_storage.service = :local
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
