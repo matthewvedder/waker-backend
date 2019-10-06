d555b3a0dc1b3d8b1cb580496a73fcc0d7836045
Matthew Vedder <matthewvedder@gmail.com>
2019-10-06 12:33:40 -0400

    down case user email before create

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :email, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   has_many :sequences
+
+  before_create :format_email
+
+  def format_email
+    self.email = self.email.downcase
+  end
 end
