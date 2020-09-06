c05e8aa40e4559b25ae619ce6252d20b7d6c4eb7
Matthew Vedder <matthewvedder@gmail.com>
2020-09-06 10:59:32 -0400

    add host in prod config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,4 +95,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  Rails.application.routes.default_url_options[:host] = 'waker.studio'
 end
