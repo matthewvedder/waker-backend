71716a4e5d487504bcfc5832136cd992f9a8a2ff
vedder <matthewvedder@gmail.com>
2020-09-22 14:23:23 -0400

    remove instance from layout on destroy

--- app/models/asana_instance.rb
+++ app/models/asana_instance.rb
@@ -1,4 +1,10 @@
 class AsanaInstance < ApplicationRecord
   belongs_to :asana
   belongs_to :sequence
+  before_destroy :remove_from_layout
+
+  def remove_from_layout
+    sequence.layout = sequence.layout.select { |instance_id| instance_id != self.id.to_s }
+    sequence.save!
+  end
 end
